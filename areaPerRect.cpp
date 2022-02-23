bfbf7cdf8c4716e6a0654c490a4dcad107eac9a8
Ubuntu <ubuntu@ip-172-31-55-166.ec2.internal>
2022-02-23 21:24:41 +0000

    Made basicMath & areaPerRect on cpp.

---
+++ areaPerRect.cpp
@@ -0,0 +1,15 @@
+// Copyrights (c) 2022 Sarah Andrew All rights reserved.
+// .
+// Created by: Sarah
+// Created on: Feb. 22, 2022
+// This program calculates and displays the area and
+// perimeter of a rectangle with dimensions 10cm X 8cm.
+#include <iostream>
+
+int main() {
+  std::cout << "For a rectangle that has dimensions\n";
+  std::cout << "length 10cm and width 8cm:\n";
+  std::cout << "\n";
+  std::cout << "Area = " << 10 * 8 << " cm^2\n";
+  std::cout << "Perimeter = " << 2* (10 + 8) << " cm\n";
+}
