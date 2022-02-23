90136ac9ee857eb0be0e3e23bc55423f2003106e
Ubuntu <ubuntu@ip-172-31-55-166.ec2.internal>
2022-02-23 21:48:25 +0000

    Made areaPerRect and basicMath in cpp.

--- basicMath.cpp
+++ basicMath.cpp
@@ -11,6 +11,6 @@ int main() {
   std::cout << "7 - 3 = " << 7-3 << std::endl;
   std::cout << "10 / 2 = " << 10/2 << std::endl;
   std::cout << "8 - 4 / 2 = " << 8-4/2 << std::endl;
-  std::cout << "2^3 = " << pow(2,3) << std::endl;
+  std::cout << "2^3 = " << pow(2, 3) << std::endl;
   std::cout << "1 / 3 = " << float(1)/float(3) << std::endl;
 }
